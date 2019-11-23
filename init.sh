2729ba4376f3565380abdff81e0b738b15b24a48
gideonthomasd <56291811+gideonthomasd@users.noreply.github.com>
2019-11-23 21:36:28 +0000

    Update init.sh

--- init.sh
+++ init.sh
@@ -8,7 +8,7 @@ sudo pacman -S lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings pcmanfm
 sudo pacman -S networkmanager network-manager-applet
 sudo pacman -S xorg-server xorg xorg-xinit
 sudo pacman -S moka-icon-theme
-sudo pacman -S w3m fluxbox conky volumeicon
+sudo pacman -S w3m fluxbox conky volumeicon mpv youtube-dl
 sudo pacman -S pavucontrol pulseaudio lxterminal
 sudo pacman -S gmrun termite ntfs-3g slock archlinux-xdg-menu
 sudo pacman -S neofetch rofi oblogout gtk2-perl lsb-release
