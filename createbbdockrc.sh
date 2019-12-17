45359ece59dad3cce604cb19d82c12d2ca99617d
gideonthomasd <56291811+gideonthomasd@users.noreply.github.com>
2019-12-17 20:56:28 +0000

    Add files via upload

---
+++ createbbdockrc.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+echo "~/myicons$((1 + RANDOM % 6))/Menu-blue-48.png:~/jgmenu.sh::1" > .bbdockrc 
+echo "~/myicons$((1 + RANDOM % 6))/Terminal-48.png:xterm::1" >> .bbdockrc
+echo "~/myicons$((1 + RANDOM % 6))/FireFox-48.png:firefox:*Firefox" >> .bbdockrc
+echo "~/myicons$((1 + RANDOM % 6))/Terminal-48(1).png:termite::1" >> .bbdockrc
+echo "~/myicons$((1 + RANDOM % 6))/Google-Chrome-Metallic-48.png:netsurf:*netsurf" >> .bbdockrc
+echo "~/myicons$((1 + RANDOM % 6))/File-media-black-red-48.png:pcmanfm::1" >> .bbdockrc
+echo "~/myicons$((1 + RANDOM % 6))/Chess_48.png:scid:*scid" >> .bbdockrc
+echo "~/myicons$((1 + RANDOM % 6))/Software-update-48.png:pamac-manager:*pamac" >> .bbdockrc
+echo "~/myicons$((1 + RANDOM % 6))/Google-Calendar-48.png:orage:*orage" >> .bbdockrc
