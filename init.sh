7a320ae7b46ac568cab27d0c11e2fe5eb54598dc
gideonthomasd <56291811+gideonthomasd@users.noreply.github.com>
2019-12-21 22:14:50 +0000

    Update init.sh

--- init.sh
+++ init.sh
@@ -10,7 +10,7 @@ sudo pacman -S xorg-server xorg xorg-xinit
 sudo pacman -S moka-icon-theme
 sudo pacman -S w3m fluxbox conky volumeicon mpv youtube-dl
 sudo pacman -S pavucontrol pulseaudio lxterminal
-sudo pacman -S gmrun termite ntfs-3g slock archlinux-xdg-menu
+sudo pacman -S gmrun termite ntfs-3g slock
 sudo pacman -S neofetch rofi oblogout gtk2-perl lsb-release
 sudo pacman -S adapta-gtk-theme gvfs pdfmixtool xpdf netsurf
 sudo pacman -S virtualbox-guest-utils deadbeef unzip wget jgmenu orage
