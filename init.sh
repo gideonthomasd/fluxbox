071623d7b0fcd5b9d31f507fddabcb440aef33a0
gideonthomasd <56291811+gideonthomasd@users.noreply.github.com>
2019-12-24 13:33:13 +0000

    Update init.sh

--- init.sh
+++ init.sh
@@ -3,17 +3,17 @@ set -e
 
 
 sudo pacman -Syyu --noconfirm
-sudo pacman -S menumaker lxsession lxappearance arc-gtk-theme xarchiver xterm base-devel nitrogen firefox git geany
-sudo pacman -S lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings pcmanfm
-sudo pacman -S networkmanager network-manager-applet
-sudo pacman -S xorg-server xorg xorg-xinit
-sudo pacman -S moka-icon-theme
-sudo pacman -S w3m fluxbox conky volumeicon mpv youtube-dl
-sudo pacman -S pavucontrol pulseaudio lxterminal
-sudo pacman -S gmrun termite ntfs-3g slock
-sudo pacman -S neofetch rofi oblogout gtk2-perl lsb-release
-sudo pacman -S adapta-gtk-theme gvfs pdfmixtool xpdf netsurf
-sudo pacman -S virtualbox-guest-utils deadbeef unzip wget jgmenu orage
+sudo pacman -S menumaker lxsession lxappearance arc-gtk-theme xarchiver xterm base-devel nitrogen firefox git geany --noconfirm
+sudo pacman -S lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings pcmanfm --noconfirm
+sudo pacman -S networkmanager network-manager-applet --noconfirm
+sudo pacman -S xorg-server xorg xorg-xinit --noconfirm
+sudo pacman -S moka-icon-theme --noconfirm
+sudo pacman -S w3m fluxbox conky volumeicon mpv youtube-dl --noconfirm
+sudo pacman -S pavucontrol pulseaudio lxterminal --noconfirm
+sudo pacman -S gmrun termite ntfs-3g slock --noconfirm
+sudo pacman -S neofetch rofi oblogout gtk2-perl lsb-release --noconfirm
+sudo pacman -S adapta-gtk-theme gvfs pdfmixtool xpdf netsurf --noconfirm
+sudo pacman -S virtualbox-guest-utils deadbeef unzip wget jgmenu orage --noconfirm
 sudo systemctl start NetworkManager
 sudo systemctl enable NetworkManager
 sudo systemctl enable lightdm
