56f13a1c4a40b9ffcd4531c9638e7742ae266078
gideonthomasd <56291811+gideonthomasd@users.noreply.github.com>
2020-02-28 08:23:38 +0000

    Create getfromyay-arco.sh

---
+++ getfromyay-arco.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+sudo pacman -Syyu --noconfirm 
+#git clone https://aur.archlinux.org/yay.git
+#cd yay
+#makepkg -si --noconfirm
+
+#yay -S --noconfirm oblogout
+yay -S --noconfirm fbpanel
+yay -S --noconfirm oranchelo-icon-theme
+yay -S --noconfirm wmckgmail
+yay -S --noconfirm bbdock
+yay -S --noconfirm libdockapp
+yay -S --noconfirm wmmenu
+
